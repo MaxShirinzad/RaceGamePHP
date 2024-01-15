58037085a33cfef0bf31d16fc6a8e4fcdc7d086e
max.shirinzad@gmail.com <max.shirinzad@gmail.com>
2024-01-16 01:02:18 +0330

    Add composer file

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => 'ef88922c53973d2c5c82e2254aa39cec63b38a53',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'ef88922c53973d2c5c82e2254aa39cec63b38a53',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
